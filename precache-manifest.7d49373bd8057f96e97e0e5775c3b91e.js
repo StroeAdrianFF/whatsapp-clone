60eab901261535f19226d0ec106e9097577cde5b
Adrian <contact@stroe-adrian.ro>
2020-09-02 19:34:32 +0300

    Updates

---
+++ precache-manifest.7d49373bd8057f96e97e0e5775c3b91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d28699b0037e38f0a8d3c0f5ecce18",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "f6e6dedd46dc63b8c84d",
+    "url": "/whatsapp-clone/static/css/main.4cbc0e20.chunk.css"
+  },
+  {
+    "revision": "2a4a0e4693ab32de29d1",
+    "url": "/whatsapp-clone/static/js/2.6477fe49.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.6477fe49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e6dedd46dc63b8c84d",
+    "url": "/whatsapp-clone/static/js/main.ca5f6763.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);

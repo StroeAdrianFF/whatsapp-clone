43b1181a37d436b9619484f3b5f3259ac751888a
Adrian <contact@stroe-adrian.ro>
2020-09-02 10:20:37 +0300

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxbnO-BHj_wt4oqssTeKzHxdyR7z267n4",
+    authDomain: "whatsapp-clone-cdd0d.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-cdd0d.firebaseio.com",
+    projectId: "whatsapp-clone-cdd0d",
+    storageBucket: "whatsapp-clone-cdd0d.appspot.com",
+    messagingSenderId: "982682430021",
+    appId: "1:982682430021:web:c4fa7122dfa0e74b10112c",
+    measurementId: "G-6HCP04JDHC"
+};
